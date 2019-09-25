b156e1edffece2820404e0011baf87bae5111dd5
Maurice Frank <maurice.frank@posteo.de>
2019-09-25 10:10:07 +0200

    Added fingerprinting to Build

--- build.pl
+++ build.pl
@@ -6,31 +6,45 @@ use File::Basename;
 use Term::ANSIColor;
 use File::Copy;
 use Digest::MD5 qw(md5_hex);
+use Getopt::Long;
 
 my $input_directory = 'content/';
 my $output_directory = 'docs/';
 my $layout_directory = 'layout/';
-# my $root = 'file:///home/morris/src/morris-frank/morris-frank.dev/docs/';
+
+my $local = 0;
 my $root = 'https://morris-frank.dev/';
+GetOptions ('local' => \$local);
+
+if ($local) {
+    print colored("⇒ Building for local dev", "white"), "\n";
+    $root = 'file:///home/morris/src/morris-frank/morris-frank.dev/docs/';
+}
 
 build();
 exit;
 
 sub build {
-    process_content_files($input_directory, $layout_directory, $output_directory, $root);
-    process_sass($layout_directory, $output_directory);
+    my $css_link = process_sass($layout_directory, $output_directory);
+    my $skeleton = process_skeleton($layout_directory, $css_link);
+    process_content_files($input_directory, $skeleton, $output_directory, $root);
+}
+
+sub process_skeleton {
+    my ($layout_dir, $css_path) = @_;
+    my $skeleton = read_file($layout_dir . 'skeleton.html');
+    $skeleton =~ s/\{\{CSS_FILE\}\}/$css_path/g;
+    return $skeleton;
 }
 
 sub process_content_files {
-    my ($input_dir, $layout_dir, $output_dir, $root_path) = @_;
+    my ($input_dir, $skeleton, $output_dir, $root_path) = @_;
     my @input_list = glob "$input_dir*html";
 
-    my $skeleton_file = read_file($layout_dir . 'skeleton.html');
-
     foreach my $input_file (@input_list) {
-        process_content_file($input_file, $skeleton_file, $output_dir, $root_path);
+        process_content_file($input_file, $skeleton, $output_dir, $root_path);
     }
-    print colored( "Processed all content", 'green' ), "\n";
+    print colored( "⇐ Processed all content", 'green' ), "\n";
 }
 
 sub process_content_file {
@@ -76,7 +90,25 @@ sub process_sass {
     my ($layout_dir, $output_dir) = @_;
     print colored( "    Processing ${layout_dir}main.sass", 'yellow' ), "\n";
     system("node_modules/.bin/sass ${layout_dir}main.sass ${output_dir}main.css");
-    print colored( "Built ${output_dir}main.css", 'green' ), "\n";
+
+    my $fingerprint = substr(hash_file("${output_dir}main.css"), 0, 10);
+    my $save_name = "main-${fingerprint}.css";
+
+    move("${output_dir}main.css", "${output_dir}${save_name}");
+    print colored( "⇐ Built ${output_dir}main.css", 'green' ), "\n";
+    return $save_name;
+}
+
+sub hash_file {
+    my ($filename) = @_;
+    my $hash;
+    local $/ = undef;
+    open FILE, "$filename";
+    binmode FILE;
+    my $data = <FILE>;
+    close FILE;
+    $hash = md5_hex($data);
+    return $hash;
 }
 
 sub read_file {
