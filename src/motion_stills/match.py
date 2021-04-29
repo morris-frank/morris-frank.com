bd3fc76273b065e1335ce5a56747efe2d38c3438
Maurice Frank <maurice.frank@posteo.de>
2021-04-29 15:02:29 +0200

    fixed the matching algo

--- src/motion_stills/match.py
+++ src/motion_stills/match.py
@@ -1,58 +1,63 @@
+from collections import defaultdict
 from pathlib import Path
-import subprocess
-from tqdm import tqdm
-from skimage import io as skio
-from skimage import feature as skfeat
-from skimage import transform as sktrans
-from pathlib import Path
-from tqdm import tqdm
-import shutil
 import pickle
-import scipy
+import shutil
+import subprocess
 
+# from torch.nn import functional as F
+from tqdm import tqdm
+import torchvision
+import torch
+print("Finished importing")
+
+
+model = torchvision.models.vgg16(pretrained=True)
+model.normalize = torchvision.transforms.Compose([torchvision.transforms.Resize(256), torchvision.transforms.CenterCrop(224), torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+
+model.eval()
+def imread(self, path):
+        x = torchvision.io.read_image(str(path)).unsqueeze(0) / 256
+        x = self.normalize(x)
+        x = self.features(x)
+        return x[0, ...].detach(), tuple(x.shape[-2:])
+print("Set-Up torch model")
 
 dir_originals = Path("~/Movies/MS/originals").expanduser()
 dir_origin = Path("~/Movies/MS/tomatch").expanduser()
 dir_target = Path("~/Movies/MS/matched").expanduser()
 path_features = dir_originals/"features.p"
 
-
-# Create frames
+# Create fram1es
 def build_frames_from_dir(directory: Path):
-    for path in tqdm(directory.glob("*mp4")):
+    for path in tqdm(directory.glob("*mp4"), desc=f"export frames {directory}"):
         if path.with_suffix(".jpg").exists():
             continue
-        subprocess.run(["ffmpeg", "-y", "-i", path, "-vframes", "1", "-f", "image2", path.with_suffix('.jpg')])
+        subprocess.run(["ffmpeg", "-y", "-i", path, "-vframes", "1", "-f", "image2", path.with_suffix('.jpg')], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 build_frames_from_dir(dir_originals)
 build_frames_from_dir(dir_origin)
+print("Exported frames for input videos")
 
 # Create features of frames
-def imread(path):
-    img = skio.imread(path)
-    img = sktrans.resize(img, (500, 500))
-    feats = skfeat.hog(img)
-    return feats
-
-if path_features.exists():
-    with open(path_features, "rb") as f:
-        feat_originals = pickle.load(f)
+if path_features.exists(): 
+    with open(path_features, "rb") as fp:
+        dictionary = pickle.load(fp)
 else:
-    feat_originals = {}
+    dictionary = defaultdict(dict)
+    for path in tqdm(dir_originals.glob("*.jpg"), desc="Building features from originals"):
+        feat, shape = imread(model, path)
+        dictionary[shape][path.stem] = feat
 
-for path in tqdm(dir_originals.glob("*.jpg")):
-    if path.stem not in feat_originals:
-        feat_originals[path.stem] = imread(path)
-with open(path_features, "wb") as f:
-    pickle.dump(feat_originals, f)
-
+with open(path_features, "wb") as fp:
+    pickle.dump(dictionary, fp)
 
 # Find NN for movies
-def find_closest(img):
+def find_closest(dictionary, feat):
     d = 1e12
     c = "UNKNOWN"
-    for name, o in feat_originals.items():
-        _d = scipy.spatial.distance.cosine(img, o)
+    for name, o in dictionary.items():
+        # _d = F.cosine_similarity(feat, o).mean().item()
+        _d = torch.dist(feat, o)
         if _d < d:
             d = _d
             c = name
@@ -61,9 +66,11 @@ def find_closest(img):
 for path in dir_origin.glob("*.jpg"):
     if not path.with_suffix(".mp4").exists():
         continue
-    img = imread(path)
-    match, distance = find_closest(img)
+    feat, shape = imread(model, path)
+    match, distance = find_closest(dictionary[shape], feat)
     print(f"{path.stem}\t->\t{match}\t:{distance}")
-    shutil.move(path.with_suffix(".mp4"), dir_target/f"{match}.mp4")
+    while (dir_target/f"{match}.mp4").exists():
+        match += "_"
+    shutil.copy(path.with_suffix(".mp4"), dir_target/f"{match}.mp4")
 
 build_frames_from_dir(dir_target)
