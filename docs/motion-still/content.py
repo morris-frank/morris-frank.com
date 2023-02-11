63e5447117273482c18f11ffb6aa9f6d6c09e522
Maurice Frank <maurice.frank@posteo.de>
2023-02-11 13:06:19 +0100

    lil update

--- docs/motion-still/content.py
+++ docs/motion-still/content.py
@@ -1,10 +1,7 @@
-
-
-
-
 def generate():
     import yaml
     from pathlib import Path
+
     c = f"""
     <p>
     I am really in love with an app made @ Google Research called <a href="https://ai.googleblog.com/2016/06/motion-stills-create-beautiful-gifs.html">MotionStills</a>. It takes little videos that can be stabilized with some quite involved CV algorithm it seems… And the app is super slim… I assume Google will kill it in the future but until then it is super fun to use.</p>
@@ -18,14 +15,12 @@ def generate():
     for year in stills:
         c += f"<h3 id=\"{year['year']}\">{year['year']}</h3>"
         for still in year["stills"]:
-            path = bU+still
+            path = bU + still
             c += f"""
             <video class="lazy motion_still" preload="none" loop onmouseenter="this.play()" onmouseleave="this.pause()" data-poster="{path}.jpg" data-src="{path}.webm">
             <source data-src="{path}.webm" type="video/webm">
             <source data-src="{path}.mp4" type="video/mp4">
             </video>
             """
-    c += "</div>"
+    c += "</div><script> window.addEventListener('load', function () { var lazyLoadInstance = new LazyLoad({}); }, false);</script>"
     return c
-
-
