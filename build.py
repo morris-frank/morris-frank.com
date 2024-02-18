566ed318ee9f81967d033b061a26d8ed2bf76e2e
Maurice Frank <maurice.frank@posteo.de>
2024-02-18 03:38:40 +0100

    update

--- build.py
+++ build.py
@@ -20,23 +20,17 @@ with open(CONTENT_DIR / "main.css", "rb") as f:
 HEAD_FILE = "head.html"
 CONTENT_FILE = "content.html"
 PYCONTENT_FILE = "content.py"
-CONFIG_FILE = "config.json"
+# CONFIG_FILE = "config.json"
 
 PAGES = [CONTENT_DIR] + list(filter(Path.is_dir, CONTENT_DIR.iterdir()))
 
 
-def fill_layout(slug: str, layout: str, content: str, head: str, config) -> str:
+def fill_layout(slug: str, layout: str, content: str, head: str) -> str:
     layout = layout.replace("{{CONTENT}}", content)
     layout = layout.replace("{{HEAD}}", head)
 
-    if "title" in config:
-        pagetitle = f"{config['title']} - {config['base_url']}"
-    else:
-        pagetitle = config["base_url"]
+    pagetitle = f"{slug.replace('-', ' ').title()} - {BASE_URL}"
     layout = layout.replace("{{PAGETITLE}}", pagetitle)
-
-    title = config.get("title", "")
-    layout = layout.replace("{{TITLE}}", title)
 
     layout = layout.replace("{{CSSHASH}}", "" if DEV else f"integrity=\"{CSSHASH}\" crossorigin=\"anonymous\"")
     layout = layout.replace("{{ROOT}}", "/docs/" if DEV else "/")
@@ -63,11 +57,7 @@ def build_page(page: Path, layout: str) -> None:
     if (page / HEAD_FILE).exists():
         head = (page / HEAD_FILE).read_text()
 
-    config = json.loads((page / CONFIG_FILE).read_text())
-    config = config or {}
-    config = {"base_url": BASE_URL, **config}
-
-    filled = fill_layout(page.name, layout=layout, content=content, head=head, config=config)
+    filled = fill_layout(page.name, layout=layout, content=content, head=head)
 
     (page / "index.html").write_text(filled)
     print(f"Built {page.name}")
--- docs/movie-timeline/generate.py
+++ docs/movie-timeline/generate.py
@@ -4,14 +4,19 @@ from pathlib import Path
 import requests
 import json
 
-api_key = "----------"
+api_key = "95e50aa5bb5cad8bb26e597775932061"
 
 data_path = Path(__file__).parent / "data.json"
+if data_path.exists():
+    data = json.loads(data_path.read_text())
+else:
+    data = {}
+
 nMovies = 0
 nMoviesFound = 0
-data = json.loads(data_path.read_text())
-if data is None:
-    data = {}
+nMoviesGlobal = 0
+nMoviesFoundGlobal = 0
+movieSet = set()
 
 
 def get_movie(api_key, movie_title):
@@ -21,6 +26,10 @@ def get_movie(api_key, movie_title):
 
     response = requests.get(search_endpoint, params=params)
     data = response.json()
+
+    if "results" not in data:
+        print(data)
+        return None
 
     # Assuming the first result is the correct movie
     if data["results"]:
@@ -34,21 +43,22 @@ def get_movie(api_key, movie_title):
 def process_wikipage(key, data):
     global nMovies, nMoviesFound
     url = f"https://en.wikipedia.org/wiki/Category:Films_set_in_the_{key.replace(' ', '_')}"
-    print(url)
     if key not in data:
         data[key] = []
 
     wikipage = BeautifulSoup(requests.get(url).text, "html.parser")
     for group in wikipage.find_all("div", class_="mw-category-group"):
         for movie_link in group.find_all("a"):
-            if "title" not in movie_link or movie_link["title"].startswith("Category:"):
+            if "title" not in movie_link.attrs or movie_link.attrs["title"].startswith("Category:"):
                 continue
+            if movie_link.text in movieSet:
+                continue
+            movieSet.add(movie_link.text)
             nMovies += 1
             meta = get_movie(api_key, movie_link.text)
             if meta:
                 nMoviesFound += 1
                 data[key].append(meta)
-    print(f"Processed {key}")
     return data
 
 
@@ -72,6 +82,14 @@ for key in keys:
     if key not in data or len(data[key]) == 0:
         data = process_wikipage(key, data)
         data_path.write_text(json.dumps(data, indent=4))
-print(f"Processed {nMoviesFound/nMovies:%} out of {nMovies} movies")
+        if nMovies > 0:
+            print(f"[{key}] Processed {nMoviesFound/nMovies:%} out of {nMovies} movies")
+        else:
+            print(f"[{key}] No movies found")
+    nMoviesGlobal += nMovies
+    nMoviesFoundGlobal += nMoviesFound
+    nMovies = 0
+    nMoviesFound = 0
+print(f"Processed {nMoviesFoundGlobal/nMoviesGlobal:%} out of {nMoviesGlobal} movies")
 
 # %%
