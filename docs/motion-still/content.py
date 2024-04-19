7b6a6a67b00d468a6736cf87d33744154129ba1d
Maurice Frank <maurice.frank@posteo.de>
2024-04-19 16:09:27 +0200

    cdn

--- docs/motion-still/content.py
+++ docs/motion-still/content.py
@@ -9,7 +9,7 @@ def generate():
     <div class="motion_still_container">
     """
 
-    bU = "https://f003.backblazeb2.com/file/motionstills/"
+    bU = "https://cdn.maurice-frank.com/motionstills/"
     stills = json.loads(Path("motionstills.json").read_text())
 
     for year in stills:
