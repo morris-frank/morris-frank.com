4da48e59a29f2f41a06053936d5992bba6e370bc
Maurice Frank <maurice.frank@posteo.de>
2024-12-28 17:32:49 +0100

    updatr

--- build.py
+++ build.py
@@ -1,68 +1,56 @@
 import hashlib
+import importlib.util
+import itertools
 import os
 from pathlib import Path
 
-BASE_URL = "maurice-frank.com"
-
-DEV = "--dev" in os.sys.argv
-
-CONTENT_DIR = Path("./docs")
-LAYOUT = (CONTENT_DIR / "__layout.html").read_text()
-
-with open(CONTENT_DIR / "main.css", "rb") as f:
-    CSSHASH = hashlib.file_digest(f, "sha256").hexdigest()[:10]
-
-for cssFile in CONTENT_DIR.glob("*.css"):
-    if cssFile.stem != "main":
-        cssFile.unlink()
-CONTENT_DIR.joinpath("main.css").with_stem(CSSHASH).write_text(CONTENT_DIR.joinpath("main.css").read_text())
+isDEV = "--dev" in os.sys.argv
+NAME = "Maurice Frank"
+DOCS_DIR = (Path(__file__).parent / "docs").absolute()
+# URL_ROOT = DOCS_DIR.as_uri() if isDEV else "http://maurice-frank.com"
+URL_ROOT = "/docs" if isDEV else "http://maurice-frank.com"
 
 
-HEAD_FILE = "head.html"
-CONTENT_FILE = "content.html"
-PYCONTENT_FILE = "content.py"
-# CONFIG_FILE = "config.json"
-
-PAGES = [CONTENT_DIR] + list(filter(Path.is_dir, CONTENT_DIR.iterdir()))
-
-
-def fill_layout(slug: str, layout: str, content: str, head: str) -> str:
-    layout = layout.replace("{{CONTENT}}", content)
-    layout = layout.replace("{{HEAD}}", head)
-
-    pagetitle = f"{slug.replace('-', ' ').title()} - {BASE_URL}"
-    layout = layout.replace("{{PAGETITLE}}", pagetitle)
-
-    layout = layout.replace("{{CSSHASH}}", CSSHASH)
-    layout = layout.replace("{{ROOT}}", "/docs/" if DEV else "/")
-
-    layout = layout.replace(f'href="/{slug}"', f'href="/{slug}" class="active" ')
-    return layout
+def update_css() -> str:
+    css_files = sorted(DOCS_DIR.glob("*.css"))
+    css = "\n".join(f.read_text() for f in css_files)
+    hash = hashlib.sha256(css.encode("utf-8")).hexdigest()[:10]
+    [f.unlink() for f in css_files]
+    (DOCS_DIR / f"{hash}.css").write_text(css)
+    return hash
 
 
-def build_page(page: Path, layout: str) -> None:
-    if not (page / CONTENT_FILE).exists() and not (page / PYCONTENT_FILE).exists():
-        print(f"Skipping {page.name} (no {CONTENT_FILE}, {PYCONTENT_FILE})")
-        return
-
-    if (page / PYCONTENT_FILE).exists():
-        cdir = os.getcwd()
-        os.chdir(page)
-        exec(Path(PYCONTENT_FILE).read_text(), {}, locals())
-        content = locals()["generate"]()
-        os.chdir(cdir)
-    else:
-        content = (page / CONTENT_FILE).read_text()
-
-    head = ""
-    if (page / HEAD_FILE).exists():
-        head = (page / HEAD_FILE).read_text()
-
-    filled = fill_layout(page.name, layout=layout, content=content, head=head)
-
-    (page / "index.html").write_text(filled)
-    print(f"Built {page.name}")
+def exec_content_module(path: Path):
+    spec = importlib.util.spec_from_file_location(path.parent.name.replace("-", "_"), path)
+    if spec is None:
+        print(f"Couldn't spec_from_file_location for {path}")
+        return ""
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    if not hasattr(module, "generate"):
+        print(f"No generate() function in {path}")
+        return ""
+    return module.generate()
 
 
-for page in PAGES:
-    build_page(page, LAYOUT)
+CSSHASH = update_css()
+LAYOUT = (DOCS_DIR / "__layout.html").read_text().replace("{{ROOT}}", URL_ROOT + "/").replace("{{CSSHASH}}", CSSHASH)
+
+for content_file in itertools.chain(DOCS_DIR.rglob("content.html"), DOCS_DIR.rglob("content.py")):
+    os.chdir(content_file.parent)
+
+    header_file = content_file.with_name("head.html")
+    index_file = content_file.with_name("index.html")
+
+    slug = content_file.parent.name
+    pagetitle = f"{slug.replace('_', ' ').title()} - {NAME}"
+    content = exec_content_module(content_file) if content_file.suffix == ".py" else content_file.read_text()
+    head = header_file.read_text() if header_file.exists() else ""
+
+    output = LAYOUT
+    output = output.replace("{{PAGETITLE}}", pagetitle)
+    output = output.replace("{{CONTENT}}", content)
+    output = output.replace("{{HEAD}}", head)
+    output = output.replace(f'href="/{slug}"', f'href="/{slug}" class="active" ')
+    index_file.write_text(output)
+    print(f"Built {content_file.parent.name}")
